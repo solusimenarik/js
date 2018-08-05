10e4679ba6e91d316db3ee82921ac4603d9007c9
solusimenarik <42022483+solusimenarik@users.noreply.github.com>
2018-08-05 11:49:02 +0700

    Add files via upload

---
+++ pro-sidebar-sticky.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){var i=jQuery(window).width();function e(){jQuery("#sidebar-sticky").stick_in_parent({parent:"#wrapper",offset_top:70})}i<768?jQuery("#sidebar-sticky").trigger("sticky_kit:detach"):e(),jQuery(window).resize(function(){(i=jQuery(window).width())<768?jQuery("#sidebar-sticky").trigger("sticky_kit:detach"):e()})});
